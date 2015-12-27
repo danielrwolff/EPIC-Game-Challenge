de6d9615dc7ae38afa3ec34814a2d22152c4a472
Daniel Wolff <wolffd@mcmaster.ca>
2015-12-26 23:22:41 -0500

    Part of punch attack done.

--- game.py
+++ game.py
@@ -19,11 +19,15 @@ class GameManager :
         self.P1DOWN = pygame.K_DOWN
         self.P1LEFT = pygame.K_LEFT
         self.P1RIGHT = pygame.K_RIGHT
+        self.P1PUNCH = pygame.K_COMMA
+        self.P1KICK = pygame.K_PERIOD
 
         self.P2UP = pygame.K_w
         self.P2DOWN = pygame.K_s
         self.P2LEFT = pygame.K_a
         self.P2RIGHT = pygame.K_d
+        self.P2PUNCH = pygame.K_LSHIFT
+        self.P2KICK = pygame.K_CAPSLOCK
 
         self.WIDTH = SIZE[0]
         self.HEIGHT = SIZE[1]
@@ -72,7 +76,18 @@ class GameManager :
         :param key: pygame.key event.
         :return: None
         '''
-        if key == self.P1UP :
+
+        if key == self.P1KICK :
+            self.player1.kick()
+        elif key == self.P1PUNCH :
+            self.player1.punch()
+
+        elif key == self.P2KICK :
+            self.player2.kick()
+        elif key == self.P2PUNCH :
+            self.player2.punch()
+
+        elif key == self.P1UP :
             self.player1.jump()
         elif key == self.P1DOWN :
             print key
@@ -81,7 +96,7 @@ class GameManager :
         elif key == self.P1RIGHT :
             self.player1.goDirection('R')
 
-        if key == self.P2UP :
+        elif key == self.P2UP :
             self.player2.jump()
         elif key == self.P2DOWN :
             print key
@@ -91,20 +106,24 @@ class GameManager :
             self.player2.goDirection('R')
 
     def doKeyUp(self, key) :
-
-        if key == self.P1UP :
-            print key
-        elif key == self.P1DOWN :
-            print key
-        elif key == self.P1LEFT :
+        '''
+        Manage a key released.
+        :param key: pygame.key event.
+        :return: None
+        '''
+        #if key == self.P1UP :
+        #    print key
+        #elif key == self.P1DOWN :
+        #    print key
+        if key == self.P1LEFT :
             self.player1.stop()
         elif key == self.P1RIGHT :
             self.player1.stop()
 
-        if key == self.P2UP :
-            print key
-        elif key == self.P2DOWN :
-            print key
+        #elif key == self.P2UP :
+        #    print key
+        #elif key == self.P2DOWN :
+        #    print key
         elif key == self.P2LEFT :
             self.player2.stop()
         elif key == self.P2RIGHT :
@@ -113,7 +132,7 @@ class GameManager :
     def getImages(self, pygame) :
         '''
         Rerieve all of the image files.
-        :param pygame: pygame
+        :param pygame: Pygame instance.
         :return: None
         '''
 
@@ -128,6 +147,10 @@ class GameManager :
         rightAnim.append(pygame.image.load(os.path.join("data", "Right", "Fall_Right2.png")).convert_alpha())
         rightAnim.append(pygame.image.load(os.path.join("data", "Right", "Fall_Right3.png")).convert_alpha())
 
+        rightAnim.append(pygame.image.load(os.path.join("data", "Right", "Punch_Right1.png")).convert_alpha())
+        rightAnim.append(pygame.image.load(os.path.join("data", "Right", "Punch_Right2.png")).convert_alpha())
+        rightAnim.append(pygame.image.load(os.path.join("data", "Right", "Punch_Right3.png")).convert_alpha())
+
         leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Stationary_Left.png")).convert_alpha())
         for i in range(1, 7) :
             leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Walk_Left" + str(i) + ".png")).convert_alpha())
@@ -135,6 +158,10 @@ class GameManager :
         leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Fall_Left.png")).convert_alpha())
         leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Fall_Left2.png")).convert_alpha())
         leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Fall_Left3.png")).convert_alpha())
+
+        leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Punch_Left1.png")).convert_alpha())
+        leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Punch_Left2.png")).convert_alpha())
+        leftAnim.append(pygame.image.load(os.path.join("data", "Left", "Punch_Left3.png")).convert_alpha())
 
         return [leftAnim, rightAnim]
 
@@ -174,6 +201,7 @@ class GameObject :
         return self.height
 
 
+
 class Sprite (GameObject) :
 
     def __init__(self, anim, xPos, yPos, width, height, col) :
@@ -194,6 +222,9 @@ class Sprite (GameObject) :
 
         self.direction = 'L'
         self.move = False
+        self.attack = False
+        self.attackType = 'NA'
+        self.attackStages = [[10, 12, 14, 16],[]]
         self.grounded = True
         self.allowDoubleJump = True
         self.animStage = 0
@@ -202,11 +233,13 @@ class Sprite (GameObject) :
         Stage 1 - 2: Start running
         Stage 3 - 6: Running animation
         Stage 7 - 9: Falling animation
+        Stage 10 - 12: Punch animation 1
         '''
         self.frameDelay = 3
         self.frameCount = 0
 
         self.groundSpeed = 5
+        self.dashDistance = 6/3
         self.fallSpeedX = 3
         self.jumpSpeed = -15
 
@@ -236,12 +269,15 @@ class Sprite (GameObject) :
 
         self.vel = physics.applyGravity(self.vel)
 
-        if self.animStage >= 2 or self.animStage == 7 :
+        if 2 <= self.animStage <= 9 :
             self.xPos += self.vel.getMagX()
+        elif 10 <= self.animStage <= 12 :
+            if self.direction == 'L' :
+                self.xPos += self.dashDistance * -1
+            else :
+                self.xPos += self.dashDistance
 
         self.yPos += self.vel.getMagY()
-
-        self.grounded = False
 
         # COLLISIONS
         self.checkCollisions(objs)
@@ -251,16 +287,39 @@ class Sprite (GameObject) :
             self.frameCount = 0
 
             if self.grounded :
-                # ...
-                if self.move :
+
+                # Attack animations.
+                if self.attack :
+                    self.animStage += 1
+                    for i in range(1,4) :
+                        if self.attackType == 'P'*i and self.animStage >= self.attackStages[0][i] :
+                            self.attack = False
+                            self.attackType = 'NA'
+                        #elif self.attackType == 'K'*i and self.animStage >= self.attackStages[1][i] :
+                        #    self.attack = False
+                        #    self.attackType = 'NA'
+
+
+                # Moving animations.
+                elif self.move :
                     if self.animStage >= 6 :
                         self.animStage = 3
                     else :
                         self.animStage += 1
+
+                # Sprite is stationary.
                 else :
                     self.animStage = 0
             else :
-                if self.vel.getMagY() < -3 :
+
+                # Air attack animations.
+                if self.attack :
+                    #######
+                    pass ##
+                    #######
+
+                # Air animations.
+                elif self.vel.getMagY() < -3 :
                     self.animStage = 7
                 elif self.vel.getMagY() > 3 :
                     self.animStage = 9
@@ -282,7 +341,7 @@ class Sprite (GameObject) :
             pos = camera.transToGameScreen(self.xPos + i[0], self.yPos + i[1])
             zoom = camera.zoomToGameScreen(int(i[2]), int(i[3]))
 
-            pygame.draw.rect(screen, (255,(i[1]*2),0), (pos[0], pos[1], zoom[0], zoom[1]))
+            #pygame.draw.rect(screen, (255,(i[1]*2),0), (pos[0], pos[1], zoom[0], zoom[1]))
 
         ### TEMP
 
@@ -323,12 +382,37 @@ class Sprite (GameObject) :
         '''
         self.move = False
 
+    def punch(self) :
+        self.attack = True
+        self.assignAttack('P', self.attackStages[0])
+
+    def kick(self) :
+        self.attack = True
+        #self.assignAttack('K')
+
+    def assignAttack(self, a, l) :
+        '''
+        Assign an attack to the sprite.
+        :param a: Attack type.
+        :param l: List of corresponding attack stages.
+        :return: None
+        '''
+        if self.animStage < l[0] :
+            self.attackType = a
+            self.animStage = l[0]
+        elif l[0] < self.animStage < l[1] :
+            self.attackType = 2*a
+        elif l[1] < self.animStage < l[2] :
+            self.attackType = 3*a
+
     def checkCollisions(self, objs) :
         '''
         Check for any collisions with the given objects.
         :param objs: Objects.
         :return: None
         '''
+
+        self.grounded = False
 
         for i in objs :
             boundary = i.getBoundary()
@@ -357,8 +441,6 @@ class Sprite (GameObject) :
                     self.vel.setMagY(0)
                     self.grounded = True
                     self.allowDoubleJump = True
-
-
 
     def getXCenter(self) :
         return self.getXPos() + self.getWidth()/2.0
